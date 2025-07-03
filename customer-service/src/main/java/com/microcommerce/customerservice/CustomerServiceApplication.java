d0c360dee7f617c2a0168e599478bd9ef350ad3b
pbecue <pbecue@ccmbenchmark.com>
2025-07-04 00:18:11 +0200

    Création du service gateway

--- customer-service/src/main/java/com/microcommerce/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/microcommerce/customerservice/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package com.microcommerce.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration.class})
+@ComponentScan(basePackages = "com.microcommerce.customerservice")
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
